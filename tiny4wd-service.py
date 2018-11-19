f7c05f065dbad5125f30111b03c1c6cabb7cae61
ongchinkiat <chinkiat@gmail.com>
2018-11-19 09:01:05 +0800

    camera config

--- tiny4wd-service.py
+++ tiny4wd-service.py
@@ -133,9 +133,7 @@ def setresolution(x,y):
     PiCamera.AWB_MODES = 
     'off'    'auto'    'sunlight'
     'cloudy'    'shade'    'tungsten'
-    'fluorescent'
-    'incandescent'
-    'flash'
+    'fluorescent'    'incandescent'    'flash'
     'horizon'
 """
 @app.route('/getcameraconfig', methods=('GET', 'POST'))
