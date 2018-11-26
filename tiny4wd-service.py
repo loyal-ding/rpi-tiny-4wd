c8a5816b0e92a891d388cf498c0d1a9bcae5f1e7
ongchinkiat <chinkiat@gmail.com>
2018-11-26 09:44:18 +0800

    move one motor backward

--- tiny4wd-service.py
+++ tiny4wd-service.py
@@ -138,6 +138,7 @@ def setresolution(x,y):
 def getcameraconfig():
     global camera
     global Rover
+    # X value reported by camera
     result = "X: " + str(Rover.camera_x) + " Camera Width: " + str(camera.resolution[0]) + "\n"
     result += "Y: " + str(Rover.camera_y) + " Camera Height: " + str(camera.resolution[1]) + "\n"
     result += "ISO: " + str(camera.iso) + "\n"
