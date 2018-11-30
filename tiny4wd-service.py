e04e0df39ec81459af2f51b336bf83a7e13309d8
ongchinkiat <chinkiat@gmail.com>
2018-11-30 09:10:18 +0800

    move one motor backward

--- tiny4wd-service.py
+++ tiny4wd-service.py
@@ -135,6 +135,7 @@ def setresolution(x,y):
     PiCamera.AWB_MODES = 
 	
     'off'    'auto'    'sunlight'    'cloudy'    'shade'    'tungsten'
+	
     'fluorescent'    'incandescent'    'flash'    'horizon'
 """
 @app.route('/getcameraconfig', methods=('GET', 'POST'))
