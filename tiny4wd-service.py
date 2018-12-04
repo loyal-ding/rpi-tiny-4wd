880b1235379dc9953d29be5811fae15b193803f5
ongchinkiat <chinkiat@gmail.com>
2018-12-04 10:39:14 +0800

    move one motor backward

--- tiny4wd-service.py
+++ tiny4wd-service.py
@@ -169,8 +169,7 @@ def getcameraconfig():
 	Exposure Modes:
 	
     'off'    'auto'    'night'    'nightpreview'    'backlight'    'spotlight'
-	
-    'sports'    'snow'    'beach'    'verylong'    'fixedfps'    'antishake'    'fireworks'
+	'sports'    'snow'    'beach'    'verylong'    'fixedfps'    'antishake'    'fireworks'
 	default "auto"
     '''
     result += "Exposure Mode: " + camera.exposure_mode + "\n"
